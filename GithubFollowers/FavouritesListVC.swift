9a34a8e6f8ea37b8d27fa2002473f769ec2807f9
shashwatsingh <shashwat.singh0406@gmail.com>
2025-01-20 20:05:56 +0530

    Initial setup and tab bar controller for search and favourites

---
+++ GithubFollowers/FavouritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavouritesListVC.swift
+//  GithubFollowers
+//
+//  Created by Shashwat Singh on 20/1/25.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+   
+
+}
---
+++ GithubFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GithubFollowers
+//
+//  Created by Shashwat Singh on 20/1/25.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
