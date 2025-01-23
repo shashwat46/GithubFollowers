50a86e848b499f0002ee1e6a0ca25383643f47d9
shashwatsingh <shashwat.singh0406@gmail.com>
2025-01-23 17:50:00 +0530

    chore: Created user and Follower Model

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Shashwat Singh on 23/1/25.
+//
+
+import Foundation
+
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
